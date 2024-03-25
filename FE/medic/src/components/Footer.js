7d16420915ccfdc662db6eef91d73c68c35c0a98
조아빈 <whdkqls122@naver.com>
2024-03-25 20:00:14 +0900

    [Style] Footer css 수정

--- FE/medic/src/components/Footer.js
+++ FE/medic/src/components/Footer.js
@@ -13,23 +13,27 @@ export default function Footer(){
     }
     return(
         <div className={`${style.main_footer} ${style.footer}`}>
-            <div className={style.footer_navigator}>
-                <div className={style.footer_navigatorbox}>
-                    <div className={`${style.btn_intro_advisor} ${style.footer_navigate_btn}`} onClick={btn_intro_advisor}>
-                        자문소개
-                    </div>
-                    <div className={`${style.btn_intro_company} ${style.footer_navigate_btn}`}onClick={btn_intro_company}>
-                        회사 소개
-                    </div>
-                    <div className={`${style.btn_show_map} ${style.footer_navigate_btn}`} onClick={btn_show_map}>
-                        약도
+            <div className={style.footer_wrap}>
+                <div className={style.footer_navigator}>
+                    <div className={style.footer_navigatorbox}>
+                        <div className={`${style.btn_intro_advisor} ${style.footer_navigate_btn}`} onClick={btn_intro_advisor}>
+                            자문소개
+                        </div>
+                        <div className={`${style.btn_intro_company} ${style.footer_navigate_btn}`}onClick={btn_intro_company}>
+                            회사 소개
+                        </div>
+                        <div className={`${style.btn_show_map} ${style.footer_navigate_btn}`} onClick={btn_show_map}>
+                            약도
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className={style.footer_box}>
-                1. 사업자 정보
-                2. 회사소개
-                3. 펙스 및 이메일 정보
+                <div className={style.footer_box}>
+                    <p>
+                        상호: 주식회사 한국의료자문센터 / 사업자번호:435-81-00113 / TEL: 02-834-4114 / FAX: 02-834-4115 / 이메일 : ikmcc@naver.com<br/>
+                        대표: 이세준 / 주소: 서울 관악구 난곡로 320, 5층(신림동, 세인빌딩) &nbsp;우. 08761<br/>
+                        Copyright © KMCC Corp. All Rights Reserved.
+                    </p>
+                </div>
             </div>
         </div>
     )
--- FE/medic/src/pages/Mainpage.js
+++ FE/medic/src/pages/Mainpage.js
@@ -3,10 +3,6 @@ import Footer from "../components/Footer";
 import style from '../css/Mainpage.module.css'
 
 export default function Mainpage(){
-    const btn_show_mypage = e => {
-        // 버튼 클릭 시 수행할 로직
-    }
-
     return (
         <div className={style.main_wrap}>
             <section>
